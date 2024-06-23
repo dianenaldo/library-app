23968ad81e3e6a71ea1c666d901f06c70f2c4db8
Ian Naldo <ian.naldo@ext-ev.solutions30.com>
2024-06-24 05:58:46 +0800

    set eslint defaults

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,7 +36,11 @@ const config = {
           "attributes": false
         }
       }
-    ]
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-empty-interface": "warn",
   }
 }
 module.exports = config;
